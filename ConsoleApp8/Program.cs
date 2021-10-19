c94ad47ccf29a1125f81133977fea38bab62fcef
ghkdwlghkd <84947242+ghkdwlghkd@users.noreply.github.com>
2021-10-19 14:41:39 +0900

    초기 커밋.

---
+++ ConsoleApp8/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ConsoleApp8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int A;
+            Console.WriteLine("몇단? : ");
+            A = int.Parse(Console.ReadLine());
+
+            for (int B = 1; B < 10; ++B)
+            {
+                Console.WriteLine($"{A}*{B}:{A * B}");
+            }
+        }
+    }
+}
