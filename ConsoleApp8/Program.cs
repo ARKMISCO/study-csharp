d620ffeacb844f08420787460f5e6538882c9e6b
ghkdwlghkd <84947242+ghkdwlghkd@users.noreply.github.com>
2021-10-21 15:13:22 +0900

    메서스 이해한 코드

--- ConsoleApp8/Program.cs
+++ ConsoleApp8/Program.cs
@@ -1,21 +1,18 @@
 ﻿using System;
 
-namespace ConsoleApp10
+namespace ConsoleApp1
 {
-    class Program // while 문으로 구구단 만들기
+    class Program
     {
+        static int intPlus(int a, int b)
+        {
+            int result = a + b;
+            return result;
+        }
         static void Main(string[] args)
         {
-            int i = 1; // i곱할것인지 j x i = ?
-            int j = 2; // j단 곱할것인지 j x i = ?
-
-            while (i < 10) ;
-            {
-                Console.WriteLine($"{j} x {i} = {j * i}");
-                {
-                    
-                }
-            }
+            int nPlus = intPlus(10, 20);
+            Console.WriteLine(nPlus);
         }
     }
 }
