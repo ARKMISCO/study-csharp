f5dd4271273eba94f73d4403579b445b2e876081
ghkdwlghkd <84947242+ghkdwlghkd@users.noreply.github.com>
2021-10-26 13:17:41 +0900

    직사각형 반대

--- ConsoleApp8/Program.cs
+++ ConsoleApp8/Program.cs
@@ -9,7 +9,7 @@ namespace baekjoon
     {
         static void Main()
         {
-            for (int a = 1; a < 10; a++)
+            for (int a = 10; a >= 1; a--)
             {
                 for (int b = 0; b < a; b++)
                     Console.Write("★");
