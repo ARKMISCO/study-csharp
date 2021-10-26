fda57139d9e8ca5f1edca31c27bff7d978634604
ghkdwlghkd <84947242+ghkdwlghkd@users.noreply.github.com>
2021-10-26 15:43:25 +0900

    로또 구매 ( 이상함 다시 고치기

--- ConsoleApp8/Program.cs
+++ ConsoleApp8/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using static System.Console;
+using System;
+using static System.Console;
 using System.Text;
 using System.IO;
 
@@ -9,13 +11,21 @@ namespace baekjoon
     {
         static void Main()
         {
-            for (int a = 10; a >= 1; a--)
-            {
-                for (int b = 0; b < a; b++)
-                    Console.Write("★");
+            int number; //랜덤 숫자 저장
+            int buy; // 구매 할 개수 숫자 저장
+
+            Console.Write("구매 할 로또의 개수를 구하시오 : ");
+            buy = int.Parse(Console.ReadLine());
+
+            while (buy-- != 0) {
+                Random rand = new Random();
+                for (int b = 5; b <= 6; b++)
+                {
+                    number = rand.Next(46);
+                    Console.Write(number + " ");
+                }
                 Console.Write("\n");
             }
-                //Console.WriteLine(a);
         }
     }
 }
